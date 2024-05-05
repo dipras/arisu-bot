a78af684af8ae9df3595d7ca91414fbb567ed75f
Dipras <dipras75@gmail.com>
2024-05-05 12:25:26 +0700

    Init

---
+++ index.js
@@ -0,0 +1,42 @@
+import { Client, GatewayIntentBits, Events } from "discord.js";
+import { arisuMisuhMp4, arisuNoMisuh, momoiNig } from "./assets";
+const token = process.env.SECRET_TOKEN;
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.on(Events.ClientReady, () => {
+  console.log(`Arisu is ready`);
+});
+
+client.on(Events.MessageCreate, (msg) => {
+  switch (msg.content.toLowerCase()) {
+    case "arisu":
+      msg.channel.send("Arisu pahlawan telah datang!");
+      return;
+    case "n":
+    case "nig":
+    case "nega":
+      msg.channel.send(momoiNig);
+      return;
+  }
+
+  if (msg.content.includes("arisu pisuhi dia")) {
+    const userTarget =
+      msg.mentions.repliedUser?.id || msg.mentions.users?.first().id;
+    if (!userTarget) return;
+    if (userTarget == process.env.DIPRAS_USER_ID) {
+      msg.channel.send(`Tidak boleh misuh kepada tuan Dipras >=<`);
+      msg.channel.send(arisuNoMisuh);
+      return;
+    }
+    msg.channel.send(`<@${userTarget}> asu kamu`);
+    msg.channel.send(arisuMisuhMp4);
+    return;
+  }
+});
+
+client.login(token);
